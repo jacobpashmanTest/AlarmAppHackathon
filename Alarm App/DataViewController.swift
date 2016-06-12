248500be79f536e95df7647decfd14fcb09176f7
Ari <arig@pasteurdrs-macbook-air.local>
2016-06-11 23:00:25 -0700

    sdjklsdjkxm,dfk

--- Alarm App/DataViewController.swift
+++ Alarm App/DataViewController.swift
@@ -7,16 +7,27 @@
 //
 import UIKit
 class DataViewController: UIViewController {
+    
     @IBOutlet weak var dataLabel: UILabel!
-    //@IBOutlet var Red: UIImageView!
+    @IBOutlet var red: UIImageView!
     var dataObject: String = ""
+    var redtrueQ = false
     @IBOutlet var Background: UIView!
+    
+   // var redtrueQ : Bool! = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        red.hidden = true
+
+        
         // self.Background.layer.cornerRadius = self.Background.frame.size.width / 0.2;
         //self.Background.clipsToBounds = true;
         // Do any additional setup after loading the view, typically from a nib.
         //print(pageDataGlobal[0])
+        
+        
         let swipeDown = UISwipeGestureRecognizer(target:self, action:Selector ("handleSwipes:"))
         let swipeUp = UISwipeGestureRecognizer(target:self, action:Selector ("handleSwipes:"))
         swipeDown.direction = .Down
@@ -35,11 +46,29 @@ class DataViewController: UIViewController {
             //UIView.animateWithDuration(0.6 , animations: { self.view.transform = CGAffineTransformMakeScale(0.6, 0.6) }, completion: { finish in UIView.animateWithDuration(0.6){ self.view.transform = CGAffineTransformIdentity } })
             print("down")
             
+            if redtrueQ == false{
+                red.hidden = false
+                redtrueQ = true
+            }else{
+                red.hidden = true
+                redtrueQ = false
+
+            }
+
+            
         }
         
         if (sender.direction == .Up) {
             print ("up")
-            
+           
+            if redtrueQ == false{
+                red.hidden = false
+                redtrueQ = true
+            }else{
+                red.hidden = true
+                redtrueQ = false
+                
+            }
         }
     }
 
---
+++ Alarm App/Red.swift
@@ -0,0 +1,14 @@
+//
+//  Red.swift
+//  Alarm App
+//
+//  Created by arig on 6/11/16.
+//  Copyright © 2016 Jacob-Ari. All rights reserved.
+//
+
+import UIKit
+class Red: UIViewController {
+
+    
+    
+}
